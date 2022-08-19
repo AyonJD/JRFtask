5f17988a296d9144037010b0ae00aaba89c230a8
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-20 00:27:39 +0600

    first card done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FixedHeightStory from '../StoryCards/FixedHeightStory';
+
+const Home = () => {
+    return (
+        <div>
+            <FixedHeightStory />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/StoryCards/FixedHeightStory.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { stories } from '../../data';
+import polyzon from '../../images/polyzon.png';
+
+const FixedHeightStory = () => {
+    const [storydata, setStoryData] = useState(stories);
+    console.log(storydata)
+
+    const hashTags = storydata[0].hashtags.join(', ');
+    return (
+        <div className='container mx-auto my-20'>
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 px-3 py-4 rounded-md bg-white h[400px]">
+                <div className='order-2 md:order-1'>
+                    <h1 className='md:text-2xl text-xl text-zinc-700 font-bold'>{storydata[0].title}</h1>
+                    <div className="sm:flex items-center my-5 justify-between">
+                        <div className='flex items-center'>
+                            <img className='h-10 rounded-full' src={storydata[0].author.profileImage} alt="" />
+                            <h1 className='ml-2 font-bold text-md lg:text-xl'>{storydata[0].author.name}</h1>
+                        </div>
+                        <div className='flex mt-3 sm:mt-0 items-center'>
+                            <img className='h-6' src={polyzon} alt="" />
+                            <h1 className='ml-2 font-bold text-md lg:text-xl'>{storydata[0].community.name}</h1>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="sm:flex my-4 ">
+                        <h1 className='text-gray-300 text-sm md:text-md font-medium'>{storydata[0].created}</h1>
+                        {
+                            <p className='text-gray-600 text-sm md:text-md sm:mt-0 mt-2 sm:ml-5 font-medium' >{hashTags.slice(0, 35)}...</p>
+                        }
+
+                    </div>
+                </div>
+                <div className='cover_image order-1 md:order-2'>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FixedHeightStory;

c35e0be3f742c145b7ee573904e20fb953ccac41
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-20 05:48:29 +0600

    All done

--- src/Components/AuthorProfile/AuthorProfile.js
+++ src/Components/AuthorProfile/AuthorProfile.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import { users } from '../../data';
-const AuthorProfile = () => {
-    const [authorData, setAuthorData] = useState(users);
+const AuthorProfile = ({ authorData, children }) => {
     return (
         <div className='container mx-auto mb-10'>
             <div className="px-3 py-4 rounded-md">
-                <h1 className='text-white text-2xl font-semibold'>Amaging Authors to follwo</h1>
+                <h1 className='text-white text-2xl font-semibold'>{children}</h1>
                 {
                     authorData.map((author, index) => {
                         return (
                             <div key={index} className="bg-[#383b4d] rounded-md py-1 my-3 px-2">
-                                <div className="sm:flex items-center my-5 justify-between">
-                                    <div className='flex items-center'>
-                                        <img className='h-10 rounded-full' src={author.profileImage} alt="" />
+                                <div className={`sm:flex items-center justify-between ${children === "Amaging Authors to follwo" ? "my-5" : "my-1"}`}>
+                                    <div className={`items-center ${children === "Amaging Authors to follwo" ? "flex" : "md:flex"}`}>
+                                        <img className={`${children === "Amaging Authors to follwo" ? "h-10 rounded-full" : "w-1/4 rounded-md mr-2"}`} src={author.profileImage} alt="" />
                                         <div className='ml-2'>
                                             <div className='flex items-center'>
                                                 <h1 className=' font-bold mr-2 text-white text-md lg:text-xl'>{author.name}</h1>
@@ -22,9 +21,9 @@ const AuthorProfile = () => {
                                             <p className='text-gray-400'>{author.bio.slice(0, 50)}....</p>
                                         </div>
                                     </div>
-                                    <div className='flex mt-3 sm:mt-0 items-center ml-12 sm:ml-0'>
-                                        <h1 className='text-white'>{author.summary.followers} Followers</h1>
-                                        <h1 className='text-blue-500 font-bold ml-5'>Follow</h1>
+                                    <div className={`flex mt-3 sm:mt-0 items-center sm:ml-0 ${children === "Amaging Authors to follwo" ? "ml-12" : "ml-0"}`}>
+                                        <h1 className={`text-white ${children === "Amaging Communities to follwo" && "text-sm"} `}>{author.summary.followers} Followers</h1>
+                                        <h1 className='text-blue-500 font-bold ml-5 cursor-pointer'>Follow</h1>
                                     </div>
                                 </div>
                             </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { communities, users } from '../../data';
 import AuthorProfile from '../AuthorProfile/AuthorProfile';
 import FixedHeightStory from '../StoryCards/FixedHeightStory';
 import LongStory from '../StoryCards/LongStory';
+import ShortStory from '../StoryCards/ShortStory';
 import VariableheightStory from '../StoryCards/VariableheightStory';
 
 const Home = () => {
+    const [authorData, setAuthorData] = useState(users);
+    const [comminutyData, setComminutyData] = useState(communities);
     return (
         <div className='px-4 md:px-0'>
             <FixedHeightStory />
             <VariableheightStory />
-            <AuthorProfile />
+            <AuthorProfile authorData={authorData}>Amaging Authors to follwo
+            </AuthorProfile>
             <LongStory />
+            <ShortStory />
+            <AuthorProfile authorData={comminutyData}>Amaging Communities to follwo
+            </AuthorProfile>
         </div>
     );
 };
